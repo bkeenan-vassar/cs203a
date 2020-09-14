9fc15f0e4f784e997611fa295b383e54f902533f
bkeenan-vassar <bkeenan@vassar.edu>
2020-09-14 16:07:39 -0400

    First snapshot

---
+++ lab01/Greeter.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Greeter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Greeter
+{
+    public static void main (String[] args) {
+        System.out.println("hello world");
+    }
+}
